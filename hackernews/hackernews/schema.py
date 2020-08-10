a473bcdf1ffac1ce41f8ee1166ca8967353ab506
carvin <carvinsingian@gmail.com>
2020-08-10 18:46:03 +0800

    Added JWT for authentication

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 
 import links.schema
 import users.schema
@@ -8,7 +9,10 @@ class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
 
 
 class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
-  pass
+  #creates 3 mutations for jwt
+  token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+  verify_token = graphql_jwt.Verify.Field()
+  refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
