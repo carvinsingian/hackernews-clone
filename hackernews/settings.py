fef9059687c730333a24765b4edd60b628e8009a
carvin <carvinsingian@gmail.com>
2020-08-11 16:57:09 +0800

    Removed django heroku in settings.py

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,6 +137,3 @@ AUTHENTICATION_BACKENDS = [
   'graphql_jwt.backends.JSONWebTokenBackend',
   'django.contrib.auth.backends.ModelBackend',
 ]
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
