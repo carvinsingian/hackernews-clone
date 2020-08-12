1bf650469b457a876de80b17f1425b9eee0c1f1e
carvin <carvinsingian@gmail.com>
2020-08-12 16:07:38 +0800

    Added login and sign up APIs to frontend for authentication

--- hackernews-frontend/src/components/Login.js
+++ hackernews-frontend/src/components/Login.js
@@ -1,15 +1,46 @@
 import React, { useState } from 'react'
 import { AUTH_TOKEN } from '../constants'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import { printIntrospectionSchema } from 'graphql'
 
 
-const Login = () => {
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($username:String!, $password:String!){
+    tokenAuth(username: $username, password: $password) {
+      token
+    }
+  }
+`
+
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($username: String!, $password: String!, $email:String!){
+    createUser(username: $username, password: $password, email: $email){
+      user {
+        username
+        email
+      }
+    }
+  }
+`
+
+
+const Login = (props) => {
   const [login, setLogin] = useState(true);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
 
-  const handleConfirmation = () => {
-    console.log("hello");
+  const handleConfirmation = async data => {
+    if (!login) {
+      setLogin(!login)
+      return
+    } else {
+      const { token } = data.tokenAuth
+      saveUserData(token)
+      props.history.push('/')
+    }
   }
 
   const saveUserData = token => {
@@ -22,17 +53,17 @@ const Login = () => {
       <div className="flex flex-column">
         {!login && (
           <input
-            value={username}
-            onChange={e => setUsername(e.target.value)}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
             type="text"
-            placeholder="Username"
+            placeholder="Email"
           />
         )}
         <input
-          value={email}
-          onChange={e => setEmail(e.target.value)}
+          value={username}
+          onChange={e => setUsername(e.target.value)}
           type="text"
-          placeholder="Email"
+          placeholder="Username"
         />
         <input
           value={password}
@@ -42,9 +73,17 @@ const Login = () => {
         />
       </div>
       <div className="flex mt3">
-        <div className="pointer mr2 button" onClick={() => handleConfirmation()}>
-          {login ? 'login' : 'create account'}
-        </div>
+        <Mutation
+          mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+          variables={{ email, password, username }}
+          onCompleted={data => handleConfirmation(data)}
+        >
+          {mutation => (
+            <div className="pointer mr2 button" onClick={mutation}>
+              {login ? 'login' : 'create account'}
+            </div>
+          )}
+        </Mutation>
         <div className="pointer button" onClick={() => setLogin(!login)}>
           {login ? 'need to create an account?' : 'already have an account?'}
         </div>
